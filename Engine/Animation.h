930ee5c057466f913c14c2a222c5d1920db1d01f
AShotkoski <shotkoskialex@gmail.com>
2022-11-28 20:32:28 -0800

    commented more shit!

--- Engine/Animation.h
+++ Engine/Animation.h
@@ -8,15 +8,23 @@ class Animation
 public:
 	Animation( int x, int y, int width, int height,float holdtime,
 		int nFrames, Surface& SpriteSheet, Color Chroma = Colors::Magenta );
+	//advances frames using realtime
 	void Update( float dt );
+	//draws current frame to pos
 	void Draw( const Vei2& pos, Graphics& gfx ) const;
 private:
+	//advances the frame
 	void Advance();
 private:
+	//chroma key color
 	Color chroma;
+	//reference to spritesheet, reference because cheaper memory
 	Surface& SpriteSheet;
+	//holds a rect of every frame
 	std::vector<RectI> frames;
+	//track current frame
 	int iCurrFrame = 0;
+	//holdtime is how long each frame is displayed
 	float holdTime;
 	float currTime = 0.f;
 };
--- Engine/Character.h
+++ Engine/Character.h
@@ -5,6 +5,7 @@
 class Character
 {
 private:
+	//different animation states the character can be in
 	enum class Sequence
 	{
 		WalkingLeft,
@@ -18,16 +19,22 @@ private:
 		Count
 	};
 public:
-	Character(Vec2 pos); //set each comp of animations vec with an animation, in order of seq
-	void Update( float dt ); //move by vel 
+	//set each comp of animations vec with an animation, in order of seq
+	Character(Vec2 pos);
+	//moves character by velocity, proportional to realtime
+	void Update( float dt );
+	//sets animation to be one from sequence, and then moves char by normalized direction
 	void SetDirection( const Vec2& dir );
 	void Draw( Graphics& gfx ) const;
 private:
 	Surface SpriteSheet = ( "Sprites\\Link90x90.bmp" );
+	//holds a diff animation for each state possible by enum Sequence
 	std::vector<Animation> animations;
+	//Position and Velocity stored as member vars
 	Vec2 pos;
 	Vec2 vel = { 0,0 };
 	Sequence currSeq = Sequence::StandingRight;
+	//since the direction is normalized, we use speed to control char move speed
 	float speed = 150.f;
 };
 
